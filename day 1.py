0aa83fbb7fff60d85ccecec47e3a9b83631ba026
Shubham singh thakur <61899555+ShubhamSinghThakur2907@users.noreply.github.com>
2020-09-22 19:41:33 +0530

    day one python coding 100 days of code

---
+++ day 1.py
@@ -0,0 +1,49 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('comment in python')
+comment in python
+>>> ##comment
+>>> ##keywords in python
+>>> import math from math
+SyntaxError: invalid syntax
+>>> import math as mth
+>>> x=10
+>>> if(x<10):
+	print('x is less then 10')
+
+	
+>>> else
+SyntaxError: invalid syntax
+>>> else:
+	
+SyntaxError: invalid syntax
+>>> else:print(x is grater then 10)
+SyntaxError: invalid syntax
+>>> input(x)
+10
+''
+>>> y
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    y
+NameError: name 'y' is not defined
+>>> input(y)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    input(y)
+NameError: name 'y' is not defined
+>>> ##data types int,string,boolen
+>>> ##float complex list tupple range
+>>> ##set frozenset
+>>> ##int data type is for storing natural numbers
+>>> ## float is for storig decimals
+>>> ##string is for storing alphabers
+>>> ##boolen is for true or false
+>>> #complex is for storing complex number
+>>> #list is for storing anything and can be appened
+>>> #tupple is for stornig a string and cant be appened
+>>> #range is for storing range
+>>> #sets are un indexed and are stored in curlly brackets
+>>> ##operators are symbols that carryout a perticular function  such as !@#$%^&*+_
+>>> 
