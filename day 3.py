3fa7a0e88893368f380ab4cc2e01dde5d1072066
Shubham singh thakur <61899555+ShubhamSinghThakur2907@users.noreply.github.com>
2020-09-24 21:52:36 +0530

    topple set dictionary
    bask

---
+++ day 3.py
@@ -0,0 +1,61 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ## tuples
+>>> ##tuples are stored in () and non appendeble
+>>> x=str('shubham')
+>>> x=shubham("cguslicos")
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    x=shubham("cguslicos")
+NameError: name 'shubham' is not defined
+>>> x=('gdykus,jv')
+>>> ## strings
+>>> ##strings are the type of datatypes used to store alphabets and characters.
+>>> ##(python dosent have char [character datatype] )
+>>> ## string is initiated by str
+>>> z=str('shubham')
+>>> print z
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(z)?
+>>> print(z)
+shubham
+>>> ##sets
+>>> ## sets are the data types used to store any datatype
+>>> ## sets are the collections of data type
+>>> #### sets dont have index number
+>>> #sets are initiated by {}
+>>> set{"shubham",52,fcgsbd,652}
+SyntaxError: invalid syntax
+>>> a={'gdgx',46,'dgchmj',6u}
+SyntaxError: invalid syntax
+>>> a={'gdgx',46,'dgchmj',6}
+>>> type(a)
+<class 'set'>
+>>> a[1]=11
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    a[1]=11
+TypeError: 'set' object does not support item assignment
+>>> ##c because sets dosent have index numbers
+>>> 
+>>> 
+>>> 
+>>> 
+>>> ##dictionary
+>>> ###dictionary have a value and a key
+>>> #dictionary are initiated by ['shubham':'djv']
+>>> b=['aysngxhm':'ugkjmvx']
+SyntaxError: invalid syntax
+>>> b=(gkudbjs:gduau)
+SyntaxError: invalid syntax
+>>> b=dict(gvjshs:dgkua,ghfayjhv:yfajedhv)
+SyntaxError: invalid syntax
+>>> gscyj = {"javhj":1,"yach":2}
+>>> type(gscyj)
+<class 'dict'>
+>>> keys(gscyj)
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    keys(gscyj)
+NameError: name 'keys' is not defined
+>>> 
